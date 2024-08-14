b8015b6085c82d500547d4903612da2591c047f1
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-15 01:39:09 +0200

    user

--- ecom/src/main/java/com/ecom/store/EcomApiApplication.java
+++ ecom/src/main/java/com/ecom/store/EcomApiApplication.java
@@ -2,8 +2,10 @@ package com.ecom.store;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EcomApiApplication {
 
 	public static void main(String[] args) {
