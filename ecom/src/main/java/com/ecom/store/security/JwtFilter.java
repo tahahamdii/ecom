9f9a8fd27f74d5d2110d62acad860744bf72afe2
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-16 17:57:31 +0200

    user details impl

--- ecom/src/main/java/com/ecom/store/security/JwtFilter.java
+++ ecom/src/main/java/com/ecom/store/security/JwtFilter.java
@@ -1,4 +1,21 @@
 package com.ecom.store.security;
 
-public class JwtFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull  FilterChain filterChain
+    ) throws ServletException, IOException {
+
+    }
 }
