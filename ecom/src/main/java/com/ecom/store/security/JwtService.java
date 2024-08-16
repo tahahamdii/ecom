482e536e00ccf3c9b1961a5f13104de06efd0899
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-16 18:41:53 +0200

    generating and building token

--- ecom/src/main/java/com/ecom/store/security/JwtService.java
+++ ecom/src/main/java/com/ecom/store/security/JwtService.java
@@ -1,11 +1,16 @@
 package com.ecom.store.security;
 
+import io.jsonwebtoken.Jwts;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JwtService {
+
+    private long jwtExpiration;
     public String extractUsername(String jwt) {
             return null;
     }
@@ -15,6 +20,19 @@ public class JwtService {
     }
 
     private String generateToken(Map<String,Object> claims, UserDetails userDetails) {
-        return null;
+        return buildToken(claims,userDetails,jwtExpiration);
     }
+
+    private String buildToken(Map<String, Object> claims, UserDetails userDetails, long jwtExpiration) {
+        var authorities = userDetails.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList();
+        return Jwts
+                .builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+
+    }
+
 }
