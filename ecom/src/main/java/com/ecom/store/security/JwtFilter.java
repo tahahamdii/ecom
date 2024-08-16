effc5ba06d2df59c2aef150643df7361f6ba22fc
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-16 18:02:23 +0200

    internal filter with filterchain

--- ecom/src/main/java/com/ecom/store/security/JwtFilter.java
+++ ecom/src/main/java/com/ecom/store/security/JwtFilter.java
@@ -20,5 +20,11 @@ public class JwtFilter extends OncePerRequestFilter {
             filterChain.doFilter(request,response);
             return;
         }
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt ;
+        final String userEmail;
+        if (authHeader == null && !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request,response);
+        }
     }
 }
