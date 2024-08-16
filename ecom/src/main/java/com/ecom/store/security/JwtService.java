adf31b214c02b4648d22d7216a798d82c2f42134
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-16 18:48:37 +0200

    creating siging Jwt with all the config

--- ecom/src/main/java/com/ecom/store/security/JwtService.java
+++ ecom/src/main/java/com/ecom/store/security/JwtService.java
@@ -1,16 +1,20 @@
 package com.ecom.store.security;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.security.Key;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
 public class JwtService {
 
     private long jwtExpiration;
+    private String secretKey;
     public String extractUsername(String jwt) {
             return null;
     }
@@ -23,7 +27,7 @@ public class JwtService {
         return buildToken(claims,userDetails,jwtExpiration);
     }
 
-    private String buildToken(Map<String, Object> claims, UserDetails userDetails, long jwtExpiration) {
+    private String buildToken(Map<String, Object> extraClaims, UserDetails userDetails, long jwtExpiration) {
         var authorities = userDetails.getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
@@ -32,7 +36,17 @@ public class JwtService {
                 .builder()
                 .setClaims(extraClaims)
                 .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + jwtExpiration))
+                .claim("authorities",authorities)
+                .signWith(getSignInKey())
+                .compact()
+                ;
 
+    }
+    private Key getSignInKey(){
+        byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+        return null;
     }
 
 }
