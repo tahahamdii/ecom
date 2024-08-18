bab29e51f92943065c29ead09e7aa08e2a05c887
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-18 14:31:24 +0200

    email template

--- ecom/src/main/java/com/ecom/store/email/EmailService.java
+++ ecom/src/main/java/com/ecom/store/email/EmailService.java
@@ -6,9 +6,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -38,6 +41,19 @@ public class EmailService {
                 StandardCharsets.UTF_8.name()
         );
 
+        Map<String, Object> proprities = new HashMap<>();
+        proprities.put("username", username);
+        proprities.put("confirmationUrl", confirmationUrl);
+        proprities.put("activation_code", activationCode);
+
+        Context context = new Context();
+        context.setVariables(proprities);
+
+        mimeMessageHelper.setFrom("hamdi.taha@esprit.tn");
+        mimeMessageHelper.setTo(to);
+        mimeMessageHelper.setSubject(subject);
+
+        String template = templateEngine.process(templateName,context);
     }
 
 }
