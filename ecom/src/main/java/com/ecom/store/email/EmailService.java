136adc9c81d81c3ad491a91aa5f1c65050c0fa0b
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-18 14:43:10 +0200

    activate account html template

--- ecom/src/main/java/com/ecom/store/email/EmailService.java
+++ ecom/src/main/java/com/ecom/store/email/EmailService.java
@@ -3,8 +3,10 @@ package com.ecom.store.email;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
@@ -14,11 +16,13 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class EmailService {
     private final JavaMailSender mailSender;
     private final SpringTemplateEngine templateEngine;
 
+    @Async
     public void sendEmail(
             String to,
             String username,
@@ -54,6 +58,8 @@ public class EmailService {
         mimeMessageHelper.setSubject(subject);
 
         String template = templateEngine.process(templateName,context);
+        mimeMessageHelper.setText(template,true);
+        mailSender.send(mimeMessage);
     }
 
 }
