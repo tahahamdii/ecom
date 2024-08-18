56c3c14eaa9606d94c04f8aca44758d0f355e61f
Hamdi Taha <hamdi.taha@esprit.tn>
2024-08-18 13:44:55 +0200

    register method

--- ecom/src/main/java/com/ecom/store/auth/AuthenticationService.java
+++ ecom/src/main/java/com/ecom/store/auth/AuthenticationService.java
@@ -1,7 +1,35 @@
 package com.ecom.store.auth;
 
+import com.ecom.store.role.RoleRepository;
+import com.ecom.store.user.User;
+import com.ecom.store.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AuthenticationService {
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+
+
     public void register(RegistrationRequest request) {
+        var userRole = roleRepository.findByName("USER")
+                .orElseThrow(()-> new IllegalStateException("ROLE USER WAS NOT INIT"));
+        var user = User.builder()
+                .firstname(request.getFirstName())
+                .lastname(request.getLastName())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .accountLocked(false)
+                .enabled(false)
+                .roles(List.of(userRole))
+                .build();
+        userRepository.save(user);
 
     }
 }
