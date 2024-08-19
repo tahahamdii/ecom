dba93710323607ee18426e81fc3cc6ec09460f36
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-19 21:20:19 +0200

    portrait entity1

--- ecom/src/main/java/com/ecom/store/portrait.java
+++ ecom/src/main/java/com/ecom/store/portrait.java
@@ -1,8 +1,18 @@
 package com.ecom.store;
 
+import jakarta.persistence.Column;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
+
 public class portrait {
     private Integer id;
     private String title ;
     private String artist;
+    @CreatedDate
+    @Column(nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+    private LocalDateTime lastModifiedDate;
+    private Integer createdBy;
 
 }
