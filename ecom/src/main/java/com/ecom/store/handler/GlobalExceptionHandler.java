8e1f916521cb65504b0a303e2af55b8fa446d16d
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-19 19:15:28 +0200

    disabled and bad credentials exceptions

--- ecom/src/main/java/com/ecom/store/handler/GlobalExceptionHandler.java
+++ ecom/src/main/java/com/ecom/store/handler/GlobalExceptionHandler.java
@@ -4,6 +4,8 @@ package com.ecom.store.handler;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.authentication.LockedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,6 +21,30 @@ public class GlobalExceptionHandler {
                         ExceptionResponse.builder()
                                 .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
                                 .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException(DisabledException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_DISABLED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_DISABLED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(BadCredentialsException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.BAD_CREDENTIALS.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
                                 .error(exp.getMessage())
                                 .build()
                 );
