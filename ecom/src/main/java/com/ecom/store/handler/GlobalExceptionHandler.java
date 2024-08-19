1230bf2932794e5b3b6827aeee674a3d0d5b44de
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-19 19:09:15 +0200

    locked exception

--- ecom/src/main/java/com/ecom/store/handler/GlobalExceptionHandler.java
+++ ecom/src/main/java/com/ecom/store/handler/GlobalExceptionHandler.java
@@ -1,14 +1,26 @@
 package com.ecom.store.handler;
 
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-    public ResponseEntity<ExceptionResponse> handleException(LockedException exp){
-
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
     }
 }
