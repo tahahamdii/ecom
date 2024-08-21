de25d7f86e702fef79cfe5074adf3d26d48934e2
Med Taha Hamdi <hamdi.taha@esprit.tn>
2024-08-21 20:40:18 +0200

    applicationAudit aware to knwo when and where :)

--- ecom/src/main/java/com/ecom/store/config/ApplicationAuditAware.java
+++ ecom/src/main/java/com/ecom/store/config/ApplicationAuditAware.java
@@ -1,4 +1,15 @@
 package com.ecom.store.config;
 
-public class ApplicationAuditAware {
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.util.Optional;
+
+public class ApplicationAuditAware implements AuditorAware<Integer> {
+    @Override
+    public Optional<Integer> getCurrentAuditor() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return Optional.empty();
+    }
 }
